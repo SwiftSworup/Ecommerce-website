d78b4d577778f2288baafadb6741ff16cefdeddc
Swarup Achhami <asworupc3@gmail.com>
2023-05-02 23:29:11 +0545

    Search bar working in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <div className='container container-fluid'>
         <Routes>
           <Route path="/" element={<Home />} exact /> 
+          <Route path="/search/:keyword" element={<Home />} exact /> 
           <Route path="/product/:id" element={<ProductDetails/>} />
         </Routes>
         <Footer />
